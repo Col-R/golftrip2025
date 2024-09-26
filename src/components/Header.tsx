fd7fac8e200f18f8dd6bdd68b5fc0f9727d2dccf
Col-R <2colewestin@gmail.com>
2024-09-26 15:28:12 -0500

    centered title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header: React.FC = () => {
     return (
-        <header className="p-5 flex justify-center items-center">
+        <header className="p-5 flex justify-center items-center text-center">
             <h1>Golf Trip 2025</h1>
         </header>
     );
