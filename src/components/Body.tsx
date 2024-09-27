f317e58be76069a8620c14bc584369809654ea60
Col-R <2colewestin@gmail.com>
2024-09-27 11:40:24 -0500

    added additional paddin to bottom when on mobile

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -10,13 +10,17 @@ import Dates from './Columns/Dates';
 
 
 
+
 const Body: React.FC = () => {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-32 md:gap-8 p-4 md:overflow-none">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-32 md:gap-8 p-4 md:p-8 md:overflow-none
+                        mb-8 md:mb-0">
             <Courses />
             <Hotels />
             <Cars />
             <Dates />
+
+            
         </div>
     );
 };
